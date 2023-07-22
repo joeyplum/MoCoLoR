9e11278cd4ac5d453d6cc5f0130f279a2037c6c1
Joey Plummer <joseph.plummer@cchmc.org>
2023-07-22 15:30:01 -0400

    Add res number to xdgrasp and mocolor outputs

--- recon_xdgrasp_npy.py
+++ recon_xdgrasp_npy.py
@@ -1,26 +1,30 @@
-import argparse 
+from sigpy_e.linop_e import NFTs, Diags, DLD, Vstacks
+import sigpy_e.cfl as cfl
+import nibabel as nib
+import logging
+import os
+import sigpy.mri as mr
+import sigpy_e.reg as reg
+import sigpy_e.ext as ext
+import argparse
 import sigpy as sp
 import scipy.ndimage as ndimage_c
 import numpy as np
 
 import sys
 sys.path.append("./sigpy_e/")
-import sigpy_e.cfl as cfl 
 
-import sigpy_e.ext as ext
-import sigpy_e.reg as reg
-from sigpy_e.linop_e import NFTs,Diags,DLD,Vstacks
-import sigpy.mri as mr
-import os
-import logging
 
-import nibabel as nib
-
-## IO parameters
+# IO parameters
 parser = argparse.ArgumentParser(description='XD-GRASP recon.')
 
 parser.add_argument('--res_scale', type=float, default=.75,
                     help='scale of resolution, full res == .75')
+parser.add_argument('--scan_res', type=float, default=200,
+                    help='scan matrix size')
+parser.add_argument('--recon_res', type=float, default=200,
+                    help='econ matrix size')
+
 parser.add_argument('--fov_x', type=float, default=1,
                     help='scale of FOV x, full res == 1')
 parser.add_argument('--fov_y', type=float, default=1,
@@ -34,9 +38,9 @@ parser.add_argument('--outer_iter', type=int, default=25,
                     help='Num of Iterations.')
 
 parser.add_argument('--vent_flag', type=int, default=0,
-                        help='output jacobian determinant and specific ventilation')
+                    help='output jacobian determinant and specific ventilation')
 parser.add_argument('--n_ref_vent', type=int, default=0,
-                        help='reference frame for ventilation')
+                    help='reference frame for ventilation')
 
 parser.add_argument('--device', type=int, default=0,
                     help='Computing device.')
@@ -47,6 +51,8 @@ args = parser.parse_args()
 
 #
 res_scale = args.res_scale
+scan_resolution = args.scan_res
+recon_resolution = args.recon_res
 fname = args.fname
 lambda_TV = args.lambda_TV
 device = args.device
@@ -57,58 +63,64 @@ n_ref_vent = args.n_ref_vent
 
 print('Reconstruction started.')
 
-## data loading
+# data loading
 data = np.load(os.path.join(fname, 'bksp.npy'))
 traj = np.real(np.load(os.path.join(fname, 'bcoord.npy')))
-dcf = np.sqrt(np.load(os.path.join(fname,'bdcf.npy')))
+dcf = np.sqrt(np.load(os.path.join(fname, 'bdcf.npy')))
 
 nf_scale = res_scale
-nf_arr = np.sqrt(np.sum(traj[0,0,:,:]**2,axis = 1)) 
-nf_e = np.sum(nf_arr<np.max(nf_arr)*nf_scale)
+nf_arr = np.sqrt(np.sum(traj[0, 0, :, :]**2, axis=1))
+nf_e = np.sum(nf_arr < np.max(nf_arr)*nf_scale)
 scale = fov_scale
-traj[...,0] = traj[...,0]*scale[0]
-traj[...,1] = traj[...,1]*scale[1]
-traj[...,2] = traj[...,2]*scale[2]
+traj[..., 0] = traj[..., 0]*scale[0]
+traj[..., 1] = traj[..., 1]*scale[1]
+traj[..., 2] = traj[..., 2]*scale[2]
 
-traj = traj[...,:nf_e,:]
-data = data[...,:nf_e]
-dcf = dcf[...,:nf_e]
+traj = traj[..., :nf_e, :]
+data = data[..., :nf_e]
+dcf = dcf[..., :nf_e]
 
-nphase,nCoil,npe,nfe = data.shape
-tshape = (np.int(np.max(traj[...,0])-np.min(traj[...,0]))
-          ,np.int(np.max(traj[...,1])-np.min(traj[...,1]))
-          ,np.int(np.max(traj[...,2])-np.min(traj[...,2])))
-# Or use input settings
-tshape = (int(args.fov_x), int(args.fov_y), int(args.fov_z))
+nphase, nCoil, npe, nfe = data.shape
+tshape = (np.int(np.max(traj[..., 0])-np.min(traj[..., 0])), np.int(np.max(
+    traj[..., 1])-np.min(traj[..., 1])), np.int(np.max(traj[..., 2])-np.min(traj[..., 2])))
+# Or use manual input settings
+tshape = (int(recon_resolution), int(
+    recon_resolution), int(recon_resolution))
 
-### calibration
-ksp = np.reshape(np.transpose(data,(1,0,2,3)),(nCoil,nphase*npe,nfe))
-dcf2 = np.reshape(dcf**2,(nphase*npe,nfe))
-coord = np.reshape(traj,(nphase*npe,nfe,3))
+print('Number of phases used in this reconstruction: ' + str(nphase))
+print('Number of coils: ' + str(nCoil))
+print('Number of phase encodes: ' + str(npe))
+print('Number of frequency encodes: ' + str(nfe))
 
-mps = ext.jsens_calib(ksp,coord,dcf2,device = sp.Device(0),ishape = tshape)
+# calibration
+ksp = np.reshape(np.transpose(data, (1, 0, 2, 3)), (nCoil, nphase*npe, nfe))
+dcf2 = np.reshape(dcf**2, (nphase*npe, nfe))
+coord = np.reshape(traj, (nphase*npe, nfe, 3))
+
+mps = ext.jsens_calib(ksp, coord, dcf2, device=sp.Device(0), ishape=tshape)
 S = sp.linop.Multiply(tshape, mps)
 
-### recon
+# recon
 PFTSs = []
 for i in range(nphase):
-    FTs = NFTs((nCoil,)+tshape,traj[i,...],device=sp.Device(device))
-    W = sp.linop.Multiply((nCoil,npe,nfe,),dcf[i,:,:]) 
+    FTs = NFTs((nCoil,)+tshape, traj[i, ...], device=sp.Device(device))
+    W = sp.linop.Multiply((nCoil, npe, nfe,), dcf[i, :, :])
     FTSs = W*FTs*S
     PFTSs.append(FTSs)
-PFTSs = Diags(PFTSs,oshape=(nphase,nCoil,npe,nfe,),ishape=(nphase,)+tshape)
+PFTSs = Diags(PFTSs, oshape=(nphase, nCoil, npe, nfe,),
+              ishape=(nphase,)+tshape)
 
-## preconditioner
-wdata = data*dcf[:,np.newaxis,:,:]
+# preconditioner
+wdata = data*dcf[:, np.newaxis, :, :]
 tmp = PFTSs.H*PFTSs*np.complex64(np.ones((nphase,)+tshape))
-L=np.mean(np.abs(tmp))
+L = np.mean(np.abs(tmp))
 
 
-## reconstruction
-q2 = np.zeros((nphase,)+tshape,dtype=np.complex64)
+# reconstruction
+q2 = np.zeros((nphase,)+tshape, dtype=np.complex64)
 Y = np.zeros_like(wdata)
 q20 = np.zeros_like(q2)
-res_norm = np.zeros((outer_iter,1))
+res_norm = np.zeros((outer_iter, 1))
 
 logging.basicConfig(level=logging.INFO)
 
@@ -116,17 +128,17 @@ sigma = 0.4
 tau = 0.4
 for i in range(outer_iter):
     Y = (Y + sigma*(1/L*PFTSs*q2-wdata))/(1+sigma)
-    
+
     q20 = q2
-    q2 = np.complex64(ext.TVt_prox(q2-tau*PFTSs.H*Y,lambda_TV))
+    q2 = np.complex64(ext.TVt_prox(q2-tau*PFTSs.H*Y, lambda_TV))
     res_norm[i] = np.linalg.norm(q2-q20)/np.linalg.norm(q2)
-    logging.info('outer iter:{}, res:{}'.format(i,res_norm[i]))
-    
+    logging.info('outer iter:{}, res:{}'.format(i, res_norm[i]))
+
     np.save(os.path.join(fname, 'prL.npy'), q2)
-    #np.save(os.path.join(fname, 'prL_residual_{}.npy'.format(lambda_TV)), res_norm)
-#q2 = np.load(os.path.join(fname, 'prL.npy'))
+    # np.save(os.path.join(fname, 'prL_residual_{}.npy'.format(lambda_TV)), res_norm)
+# q2 = np.load(os.path.join(fname, 'prL.npy'))
 # jacobian determinant & specific ventilation
-if vent_flag==1:
+if vent_flag == 1:
     print('Jacobian Determinant and Specific Ventilation...')
     jacs = []
     svs = []
@@ -140,7 +152,7 @@ if vent_flag==1:
     svs = np.asarray(svs)
     np.save(os.path.join(fname, 'jac_xdgrasp.npy'), jacs)
     np.save(os.path.join(fname, 'sv_xdgrasp.npy'), svs)
-    
+
     # Check whether a specified save data path exists
     results_exist = os.path.exists(fname + "/results")
 
@@ -189,10 +201,14 @@ if vent_flag==1:
     aff = translation_affine.dot(rotation_affine.dot(scaling_affine))
 
     ni_img = nib.Nifti1Image(abs(np.moveaxis(q2, 0, -1)), affine=aff)
-    nib.save(ni_img, fname + '/results/img_xdgrasp_' + str(nphase) + '_bin')
+    nib.save(ni_img, fname + '/results/img_xdgrasp_' + str(nphase) +
+             '_bin' + str(int(recon_resolution)) + '_resolution')
 
-    ni_img = nib.Nifti1Image(np.moveaxis(svs, 0, -1), affine=aff)
-    nib.save(ni_img, fname + '/results/sv_xdgrasp_' + str(nphase) + '_bin')
+    if vent_flag == 1:
+        ni_img = nib.Nifti1Image(np.moveaxis(svs, 0, -1), affine=aff)
+        nib.save(ni_img, fname + '/results/sv_xdgrasp_' +
+                 str(nphase) + '_bin' + str(int(recon_resolution)) + '_resolution')
 
-    ni_img = nib.Nifti1Image(np.moveaxis(jacs, 0, -1), affine=aff)
-    nib.save(ni_img, fname + '/results/jacs_xdgrasp_' + str(nphase) + '_bin')
+        ni_img = nib.Nifti1Image(np.moveaxis(jacs, 0, -1), affine=aff)
+        nib.save(ni_img, fname + '/results/jacs_xdgrasp_' + str(nphase) +
+                 '_bin' + str(int(recon_resolution)) + '_resolution')
