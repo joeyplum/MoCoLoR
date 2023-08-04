9c4a2e17a0ea364989fd7faffe77dfb33a32ff84
Joey Plummer <joseph.plummer@cchmc.org>
2023-08-04 18:43:07 -0400

    Update run_recons.sh

--- run_recons.sh
+++ run_recons.sh
@@ -5,27 +5,37 @@ PYTHON_EXECUTABLE=python
 
 # Function to run the binning_quantile.py script with given arguments
 run_binning_quantile() {
-    $PYTHON_EXECUTABLE binning_quantile.py --fname data/floret-740H-053/ --nbins 20 --plot 0
+    $PYTHON_EXECUTABLE binning_quantile.py --fname data/floret-740H-035c/ --nbins 20 --plot 0
 }
 
 # Function to run the recon_xdgrasp_npy.py script with given arguments
 run_recon_xdgrasp1() {
-    $PYTHON_EXECUTABLE recon_xdgrasp_npy.py data/floret-740H-053/ --lambda_TV 0.05 --vent_flag 1 --recon_res 220 --scan_res 220
+    $PYTHON_EXECUTABLE recon_xdgrasp_npy.py data/floret-740H-035c/ --lambda_TV 0.05 --vent_flag 1 --recon_res 220 --scan_res 220
 }
 
 # Function to run the recon_xdgrasp_npy.py script with given arguments
 run_recon_xdgrasp2() {
-    $PYTHON_EXECUTABLE recon_xdgrasp_npy.py data/floret-740H-053/ --lambda_TV 0.05 --vent_flag 1 --recon_res 110 --scan_res 220
+    $PYTHON_EXECUTABLE recon_xdgrasp_npy.py data/floret-740H-035c/ --lambda_TV 0.05 --vent_flag 1 --recon_res 120 --scan_res 220
+}
+
+# Function to run the recon_xdgrasp_npy.py script with given arguments
+run_recon_xdgrasp3() {
+    $PYTHON_EXECUTABLE recon_xdgrasp_npy.py data/floret-740H-035c/ --lambda_TV 0.05 --vent_flag 1 --recon_res 90 --scan_res 220
 }
 
 # Function to run the recon_mocolor_npy.py script with given arguments
 run_recon_mocolor1() {
-    $PYTHON_EXECUTABLE recon_lrmoco_vent_npy.py data/floret-740H-053/ --lambda_lr 0.05 --vent_flag 1 --recon_res 220 --scan_res 220 --mr_cflag 1
+    $PYTHON_EXECUTABLE recon_lrmoco_vent_npy.py data/floret-740H-035c/ --lambda_lr 0.05 --vent_flag 1 --recon_res 220 --scan_res 220 --mr_cflag 1
 }
 
 # Function to run the recon_mocolor_npy.py script with given arguments
 run_recon_mocolor2() {
-    $PYTHON_EXECUTABLE recon_lrmoco_vent_npy.py data/floret-740H-053/ --lambda_lr 0.05 --vent_flag 1 --recon_res 110 --scan_res 220 --mr_cflag 1
+    $PYTHON_EXECUTABLE recon_lrmoco_vent_npy.py data/floret-740H-035c/ --lambda_lr 0.05 --vent_flag 1 --recon_res 120 --scan_res 220 --mr_cflag 1
+}
+
+# Function to run the recon_mocolor_npy.py script with given arguments
+run_recon_mocolor3() {
+    $PYTHON_EXECUTABLE recon_lrmoco_vent_npy.py data/floret-740H-035c/ --lambda_lr 0.05 --vent_flag 1 --recon_res 90 --scan_res 220 --mr_cflag 1
 }
 
 echo "Running binning_quantile.py ..."
@@ -40,12 +50,20 @@ echo "Running recon_xdgrasp_npy.py ..."
 run_recon_xdgrasp2
 echo "Finished recon_xdgrasp_npy.py"
 
+echo "Running recon_xdgrasp_npy.py ..."
+run_recon_xdgrasp3
+echo "Finished recon_xdgrasp_npy.py"
+
 echo "Running recon_mocolor_npy.py ..."
 run_recon_mocolor1
 echo "Finished recon_mocolor_npy.py"
 
 echo "Running recon_mocolor_npy.py ..."
 run_recon_mocolor2
+echo "Finished recon_mocolor_npy.py"
+
+echo "Running recon_mocolor_npy.py ..."
+run_recon_mocolor3
 echo "Finished recon_mocolor_npy.py"
 
 # Now, you need to make the shell script executable. Open your terminal and navigate to the directory containing the run_recons.sh file, then run the following command:
